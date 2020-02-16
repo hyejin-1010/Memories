eb2c3e3db7f5b47b95a031b4d45f27e18dc48723
Hyejin1010 <hyepago@naver.com>
2020-02-16 23:08:38 +0900

    FEAT: put schedule/:id api

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -55,5 +55,40 @@ module.exports = function (app, Schedule) {
         });
       }
     });
-  })
+  });
+
+  // 일정 수정
+  app.put('/api/schedule/:id', function (req, res) {
+    const id = req.params.id;
+    body = req.body;
+
+    const token = req.headers['x-access-token'];
+    let decoded = jwt.decode(token);
+    if (!decoded || !decoded.uid) {
+      res.status(401).json({ success: false });
+      return;
+    }
+
+    User.findOne({ uid: decoded.uid }, { _id: true }, function(err, user) {
+      if (err || !user) { res.status(500).json({ success: false }); }
+      else {
+        Schedule.update({ _id: id }, { $set: body }, function(err) {
+          if (err) {
+            res.status(500).json({ success: false, message: err.errmsg });
+            return;
+          }
+
+          Schedule.findOne({ _id: id }, function (err, sch) {
+            if (err) { res.status(500).json({ success: false, message: err.errmsg }); }
+            else {
+              res.json({
+                success: true,
+                data: sch
+              });
+            }
+          });
+        });
+      }
+    });
+  });
 }
